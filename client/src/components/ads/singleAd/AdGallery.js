a25cb9ff647572f786f1f03d3fdeff0d379b78b5
Felix Avelar <felixavco@gmail.com>
2019-03-02 17:34:29 -0600

    Ready to deply take 3 .... ACTION!

--- client/src/components/ads/singleAd/AdGallery.js
+++ client/src/components/ads/singleAd/AdGallery.js
@@ -1,52 +1,44 @@
-import React from 'react'
-import emtpyImage from '../../../img/empty-image.png'
+import React from 'react';
+import emtpyImage from '../../../img/empty-image.png';
 
-const AdGallery = ({images}) => {
+const AdGallery = ({ images }) => {
+	let indicators = images.map((image, i) => (
+		<li key={i} data-target="#postGallery" data-slide-to="0" className={i === 0 ? 'active' : null} />
+	));
 
-  let indicators = images.map((image, i) => (
-    <li key={i} data-target="#postGallery" data-slide-to="0" className={i === 0 ? 'active' : null}></li>
-  ))
+	let gallery_Images;
 
-  let gallery_Images;
+	if (images.length !== 0) {
+		gallery_Images = images.map((image, i) => (
+			<div key={i} className={`carousel-item ${i === 0 ? 'active' : null}`}>
+				<img className="d-block w-100" src={`/${image}`} alt={image} />
+			</div>
+		));
+	} else {
+		gallery_Images = (
+			<div className="carousel-item active">
+				<img className="d-block w-100" src={emtpyImage} alt="No-image" />
+			</div>
+		);
+	}
 
-  if(images.length !== 0) {
-    gallery_Images = images.map((image, i) => (
-      <div key={i} className={`carousel-item ${i === 0 ? 'active' : null}`}>
-        <img className="d-block w-100" src={`/${image}`} alt={image}/>
-      </div>
-    ))
-  } else {
-    <div className='carousel-item active'>
-      <img className="d-block w-100" src={emtpyImage} alt="No-image"/>
-    </div>
-  }
+	return (
+		<div id="postGallery" className="carousel slide mb-3" data-ride="carousel">
+			<ol className="carousel-indicators">{indicators}</ol>
 
- 
+			<div className="carousel-inner">{gallery_Images}</div>
 
-  return (
-    <div id="postGallery" className="carousel slide mb-3" data-ride="carousel">
+			<a className="carousel-control-prev" href="#postGallery" role="button" data-slide="prev">
+				<i className="far fa-arrow-alt-circle-left" />
+				<span className="sr-only">Previous</span>
+			</a>
 
-      <ol className="carousel-indicators">
-        { indicators }
-      </ol>
+			<a className="carousel-control-next" href="#postGallery" role="button" data-slide="next">
+				<i className="far fa-arrow-alt-circle-right" />
+				<span className="sr-only">Next</span>
+			</a>
+		</div>
+	);
+};
 
-      <div className="carousel-inner">
-        { gallery_Images }
-      </div>
-
-      <a className="carousel-control-prev" href="#postGallery" role="button" data-slide="prev">
-        <i className="far fa-arrow-alt-circle-left"/>
-        <span className="sr-only">Previous</span>
-      </a>
-
-      <a className="carousel-control-next" href="#postGallery" role="button" data-slide="next">
-        <i className="far fa-arrow-alt-circle-right"/>
-        <span className="sr-only">Next</span>
-      </a>
-
-    </div>
-  )
-}
-
-export default AdGallery
-
+export default AdGallery;
