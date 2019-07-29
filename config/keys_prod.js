a123bdae234ac080b6be82a1d77ecf321f67b8cb
Felix Avelar <felixavco@gmail.com>
2019-07-29 14:34:43 -0600

    Rename ENV var from MY_HOST to HOST

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,7 +2,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
-  HOST: process.env.MY_HOST, 
+  HOST: process.env.HOST, 
   MG_API_KEY: process.env.MG_API_KEY, 
   DOMAIN: process.env.DOMAIN
 }
